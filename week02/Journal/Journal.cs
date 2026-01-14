69e415e554f408083d3a60115db2fffe146040b8
sayhitoritu <rrai@byupathway.edu>
2026-01-14 19:19:20 +0530

    add

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+    }
+
+    public void DisplayAll()
+    {
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+            Console.WriteLine();
+        }
+    }
+}
